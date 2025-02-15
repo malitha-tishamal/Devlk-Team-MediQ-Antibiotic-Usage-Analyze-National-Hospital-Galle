4d27f09e78de92862ef3ae5c3ff0b2f1858c6758
malitha-tishamal <malithatishamal@gmail.com>
2025-02-16 04:12:43 +0530

    update connection

--- includes/db-conn.php
+++ includes/db-conn.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "sql203.infinityfree.com";
-    $username = "if0_37526384";
-    $password = "malitha2003";
-    $dbname = "if0_37526384_mediq_db2";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mediq_db2";
     
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
