17f78963769d86204306fd2fe4f15533a431387c
malitha-tishamal <malithatishamal@gmail.com>
2025-07-11 04:10:06 +0530

    fixed user side bar issue

--- includes/user-sidebar.php
+++ includes/user-sidebar.php
@@ -11,11 +11,11 @@
         </li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav1" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-capsule"></i></i></i><span>Antibiotics</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
 
-            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="components-nav1" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="pages-release-antibiotic.php">
                         <i class="bi bi-circle"></i><span>Dispensing Antibiotics</span>
